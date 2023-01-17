04061b8b28981ff273a26c99c3b9306a70bffd6b
lupuuss <damian.baczynski@gmail.com>
2023-01-17 22:19:16 +0100

    Move InitAction to separate class

---
+++ shared/src/commonMain/kotlin/com/github/lupuuss/diskount/InitAction.kt
@@ -0,0 +1,5 @@
+package com.github.lupuuss.diskount
+
+import dev.redukt.core.Action
+
+internal object InitAction : Action
--- shared/src/commonMain/kotlin/com/github/lupuuss/diskount/InitMiddleware.kt
+++ shared/src/commonMain/kotlin/com/github/lupuuss/diskount/InitMiddleware.kt
@@ -3,15 +3,12 @@ package com.github.lupuuss.diskount
 import com.github.lupuuss.diskount.slices.Destination
 import com.github.lupuuss.diskount.slices.DestinationType
 import com.github.lupuuss.diskount.slices.NavigationAction
-import dev.redukt.core.Action
 import dev.redukt.core.coroutines.coroutineScope
 import dev.redukt.core.middleware.translucentMiddleware
 import dev.redukt.data.DataSourceAction
 import dev.redukt.data.DataSourcePayload
 import dev.redukt.data.callDataSource
 import kotlinx.coroutines.launch
-
-internal object InitAction : Action
 
 internal val initMiddleware = translucentMiddleware<AppState> { action ->
     if (action !is InitAction) return@translucentMiddleware
