c8fc3e7d622fdd87403af53979ffd1d94f932faf
lupuuss <damian.baczynski@gmail.com>
2023-02-26 00:13:46 +0100

    Improve Router

--- androidApp/src/main/java/com/github/lupuuss/diskount/android/Router.kt
+++ androidApp/src/main/java/com/github/lupuuss/diskount/android/Router.kt
@@ -39,28 +39,21 @@ fun <D : Any> Router(
 }
 
 @Composable
-fun <T> rememberRef(): MutableState<T?> {
-    // for some reason it always recreated the value with vararg keys,
-    // leaving out the keys as a parameter for remember for now
-    return remember() {
-        object : MutableState<T?> {
-            override var value: T? = null
-
-            override fun component1(): T? = value
-
-            override fun component2(): (T?) -> Unit = { value = it }
-        }
-    }
-}
-
-@Composable
 fun <T> rememberPrevious(
     current: T,
-    shouldUpdate: (prev: T?, curr: T) -> Boolean = { a: T?, b: T -> a != b },
 ): T? {
-    val ref = rememberRef<T>()
+    var value: T? by remember { silentMutableStateOf(null) }
     SideEffect {
-        if (shouldUpdate(ref.value, current)) ref.value = current
+        if (value != current) value = current
     }
-    return ref.value
+    return value
+}
+
+// It does not cause the re-render
+private fun <T> silentMutableStateOf(initialValue: T): MutableState<T> = object : MutableState<T> {
+    override var value: T = initialValue
+
+    override fun component1(): T = value
+
+    override fun component2(): (T) -> Unit = { value = it }
 }
